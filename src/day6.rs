3927dd9beedea07a8899da26bb9037767e12d343
Daniel Goldstein <danielgold95@gmail.com>
2020-12-06 11:43:42 -0500

    simplify

--- src/day6.rs
+++ src/day6.rs
@@ -13,18 +13,12 @@ pub fn day6() {
     let counts: usize = input.split("\n\n").map(count_unique_any_answer).sum();
     println!("The total number of questions is: {}", counts);
 
-    let input = read_input_to_string(String::from("day6"));
     let counts: usize = input.split("\n\n").map(count_unique_all_answer).sum();
     println!("Actually, the total number of questions is: {}", counts);
 }
 
 fn count_unique_any_answer(group_answers: &str) -> usize {
-    // flat_map is the best
-    group_answers
-        .lines()
-        .flat_map(|s| s.chars())
-        .collect::<HashSet<char>>()
-        .len()
+    group_answers.chars().collect::<HashSet<char>>().len()
 }
 
 fn count_unique_all_answer(group_answers: &str) -> usize {
