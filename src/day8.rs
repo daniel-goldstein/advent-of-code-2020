b44f848278d3ddee497e79ac1c3e6594b262a929
Daniel Goldstein <danielgold95@gmail.com>
2020-12-08 10:37:01 -0500

    part 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,22 +1,24 @@
 use crate::utils::read_input_to_string;
+use std::collections::HashSet;
 
 pub fn day8() {
     println!("DAY 8");
-    println!("The value of the accumulator is: {}", run());
+    println!("The value of the accumulator is: {}", part1());
+    println!("The value of the accumulator after fixing is: {}", part2());
 }
 
-fn run() -> i32 {
+fn part1() -> i32 {
     let input = read_input_to_string(String::from("day8"));
-    let mut program = read_program(&input);
+    let program = read_program(&input);
     let mut accumulator: i32 = 0;
     let mut idx = 0;
+    let mut executed = HashSet::new();
     loop {
-        println!("The accumulator is: {}", accumulator);
-        let (instruction, val, executed) = program[idx];
-        if executed {
+        let (instruction, val) = program[idx];
+        if executed.contains(&idx) {
             return accumulator;
         }
-        program[idx].2 = true;
+        executed.insert(idx);
         match instruction {
             "nop" => idx += 1,
             "jmp" => idx = ((idx as i32) + val) as usize,
@@ -29,7 +31,59 @@ fn run() -> i32 {
     }
 }
 
-fn read_program(input: &str) -> Vec<(&str, i32, bool)> {
+fn part2() -> i32 {
+    let input = read_input_to_string(String::from("day8"));
+    let mut program = read_program(&input);
+    let mut line = 0;
+    while line < program.len() {
+        match program[line].0 {
+            "jmp" => {
+                program[line].0 = "nop";
+                if let Some(acc) = run_program(&program) {
+                    return acc;
+                }
+                program[line].0 = "jmp";
+            },
+            "nop" => {
+                program[line].0 = "jmp";
+                if let Some(acc) = run_program(&program) {
+                    return acc;
+                }
+                program[line].0 = "nop";
+            },
+            _ => {},
+        }
+        line += 1;
+    }
+
+    panic!("Nothing worked!");
+}
+
+fn run_program(program: &Vec<(&str, i32)>) -> Option<i32> {
+    let mut accumulator: i32 = 0;
+    let mut idx = 0;
+    let mut executed = HashSet::new();
+    while idx < program.len() {
+        let (instruction, val) = program[idx];
+        if executed.contains(&idx) {
+            return None;
+        }
+        executed.insert(idx);
+        match instruction {
+            "nop" => idx += 1,
+            "jmp" => idx = ((idx as i32) + val) as usize,
+            "acc" => {
+                accumulator += val;
+                idx += 1;
+            }
+            _ => panic!("Unknown instruction: {}", instruction)
+        }
+    }
+
+    Some(accumulator)
+}
+
+fn read_program(input: &str) -> Vec<(&str, i32)> {
     input
         .lines()
         .map(|line| {
@@ -37,7 +91,6 @@ fn read_program(input: &str) -> Vec<(&str, i32, bool)> {
             (
                 line_iter.next().unwrap(),
                 line_iter.next().unwrap().parse::<i32>().unwrap(),
-                false,
             )
         })
         .collect()
