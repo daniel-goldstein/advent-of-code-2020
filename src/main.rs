b56173c8efb75fbe7328d97fd6c94ee7654e2d3b
Daniel Goldstein <danielgold95@gmail.com>
2020-12-10 21:56:47 -0500

    day 10

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,5 @@
-use advent_of_code_2020::day1::day1;
-use advent_of_code_2020::day2::day2;
-use advent_of_code_2020::day3::day3;
-use advent_of_code_2020::day4::day4;
-use advent_of_code_2020::day5::day5;
-use advent_of_code_2020::day6::day6;
-use advent_of_code_2020::day7::day7;
-use advent_of_code_2020::day8::day8;
-use advent_of_code_2020::day9::day9;
+use advent_of_code_2020::day10::day10;
 
 fn main() {
-    println!();
-    day1();
-    println!();
-    day2();
-    println!();
-    day3();
-    println!();
-    day4();
-    println!();
-    day5();
-    println!();
-    day6();
-    println!();
-    day7();
-    println!();
-    day8();
-    println!();
-    day9();
+    day10();
 }
